9fededfbf4f6378ec138e66eb9f0be57351b1f0a
Daniel Monsalve <dannymonsalveb@gmail.com>
2025-04-13 21:38:01 -0500

    CRUD

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 //importamos al framework express
 import express from "express";
+import cors from "cors"
 import categoriasRoutes from "./routes/categorias.routes.js"
 
 //asignamos a app toda funcionalidad para mi server web
@@ -8,6 +9,7 @@ const app = express();
 //setear un puerto a mi web server
 app.set("port",5000)
 
+app.use(cors());
 //routes
 app.use("/api/categorias",categoriasRoutes)
 //hacemos disponible a mi server app para toda la aplicacion
